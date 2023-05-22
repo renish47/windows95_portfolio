76ad5f309b6e6d6f94cea83409d9df29950626c8
renish47 <ineroff77@gmail.com>
2023-05-22 22:00:50 +0530

    update project descriptions

--- src/content/projects.js
+++ src/content/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     {
         name: "Tees For You",
         imagePath: "/assets/projectSnaps/project1a.jpg",
-        description: "An E-Commerce website with modern UI to buy T-Shirts. This is my first complete website project",
+        description: "An E-Commerce website with modern UI to buy T-Shirts. This is my first complete website project.\n (This isn't designed for mobile view yet)",
         githubLink: "https://github.com/renish47/TeesForYou-MiniProject1",
         websiteLink: "https://teesforyou.netlify.app/",
         techStack: [html, css, js]
@@ -11,7 +11,7 @@ export const projects = [
     {
         name: "Book My Doc",
         imagePath: "/assets/projectSnaps/project2a.jpg",
-        description: "A Doctor appointment booking app which provides functionalities to book and manage appointments. This is my first Full-Stack project",
+        description: "A Doctor appointment booking app which provides functionalities to book and manage appointments. This is my first Full-Stack project.",
         githubLink: "https://github.com/renish47/BookMyDoc",
         websiteLink: "https://bookmydoc.netlify.app/",
         techStack: [react, bootstrap, expressJs, mongoDb]
@@ -19,7 +19,7 @@ export const projects = [
     {
         name: "Portfolio",
         imagePath: "/assets/projectSnaps/project3a.jpg",
-        description: "My web portfolio whose design is inspired based on popular Windows-95 operating system's look ",
+        description: "My web portfolio whose design is inspired based on popular Windows-95 operating system's look.",
         githubLink: "https://github.com/renish47/portfolio",
         websiteLink: "https://renishkar-portfolio.netlify.app/",
         techStack: [react, tailwindCss]
